33f72bf266dc633b8f64b81a2397e535365fe5bd
Saurav0398 <61222002+Saurav0398@users.noreply.github.com>
2023-05-06 22:19:31 +0530

    Updating

--- src/app/contact.service.ts
+++ src/app/contact.service.ts
@@ -8,7 +8,7 @@ import { Contact } from './contact';
 })
 export class ContactService {
 
-  private baseUrl="http://a2dd8f4fe4265469c97db8fb4f68d5fa-606287383.ap-south-1.elb.amazonaws.com"
+  private baseUrl="http://ab7f0ea892e2945ce91379f305ca66e8-1586098.ap-south-1.elb.amazonaws.com"
 
   constructor(private httpClient:HttpClient) { }
 
